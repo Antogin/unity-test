fe0d9d52f92ba6f42b07475fbaad9d2f3174e1d0
Oricy <d.duvernois@student.isartdigital.com>
2018-12-14 09:37:55 +0100

    basic gameplay

--- Assets/killbox.cs
+++ Assets/killbox.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class killbox : MonoBehaviour {
 
+    private bool steak = false;
+    private bool salad = false;
+    private bool onion = false;
+    private bool cheese = false;
+    private bool bread = false;
+
+    private GameObject pot;
+    private GameObject burger;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        pot = GameObject.Find("marm");
+        burger = GameObject.Find("myBurger");
+        burger.SetActive(false);
+
+    }
 	    
 	// Update is called once per frame
 	void Update () {
@@ -16,9 +28,34 @@ public class killbox : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.tag);
-        if(other.gameObject.tag == "food"){
+        if(other.gameObject.name == "Steak_Cooked")
+        {
+            steak = true;
+        }
+        if (other.gameObject.name == "Salad")
+        {
+            salad = true;
+        }
+        if (other.gameObject.name == "Onion")
+        {
+            onion = true;
+        }
+        if (other.gameObject.name == "Cheese_02")
+        {
+            cheese = true;
+        }
+        if (other.gameObject.name == "Bread")
+        {
+            bread = true;
+        }
+        if (other.gameObject.tag == "food"){
             Destroy(other.gameObject);
         }
+        if(steak && salad && onion && cheese && bread)
+        {
+            pot.SetActive(false);
+            burger.SetActive(true);
+        }
+
     }
 }
